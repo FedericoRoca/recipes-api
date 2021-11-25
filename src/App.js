19f1745018ac8f649e3724e616660618b7f1cbcc
Federico Roca <roca.fedee@gmail.com>
2021-11-25 15:21:58 -0300

    API request fixed

--- src/App.js
+++ src/App.js
@@ -14,12 +14,12 @@ class App extends Component {
     const recipeName = e.target.elements.recipeName.value;
     e.preventDefault();
     const api_call = await fetch(
-      `https://cors-anywhere.herokuapp.com/http://food2fork.com/api/search?key=${API_KEY}&q=${recipeName}&count=10`,
+      `https://cors-anywhere.herokuapp.com/http://recipesapi.herokuapp.com/api/v2/recipes?q=${recipeName}&page=1`,
     );
 
     const data = await api_call.json();
     this.setState({recipes: data.recipes});
-    console.log(this.state.recipes);
+    console.log('fede', this.state.recipes);
   };
   componentDidMount = () => {
     const json = localStorage.getItem('recipes');
--- src/components/Recipe.js
+++ src/components/Recipe.js
@@ -11,22 +11,23 @@ class Recipe extends React.Component {
   componentDidMount = async () => {
     const title = this.props.location.state.recipe;
     const req = await fetch(
-      `https://cors-anywhere.herokuapp.com/http://food2fork.com/api/search?key=${API_KEY}&q=${title}`,
+      `https://cors-anywhere.herokuapp.com/http://recipesapi.herokuapp.com/api/v2/recipes?q=${title}`,
     );
 
     const res = await req.json();
     this.setState({activeRecipe: res.recipes[0]});
-    console.log(this.state.activeRecipe);
+    console.log('hola', this.state.activeRecipe);
   };
   render() {
     const recipe = this.state.activeRecipe;
+    console.log('recipe', recipe);
     return (
       <div className="container">
         {this.state.activeRecipe.length !== 0 && (
           <div className="active-recipe">
             <img
               className="active-recipe__img"
-              src={recipe.image_url}
+              src={recipe.imageUrl}
               alt={recipe.title}
             />
             <h3 className="active-recipe__title">{recipe.title}</h3>
